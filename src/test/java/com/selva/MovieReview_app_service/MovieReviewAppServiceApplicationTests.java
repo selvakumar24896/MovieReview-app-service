6b04998756cfb1d31638a62a6b06e4e0abbe9a1c
selvakumar24896 <selvakumar24896@gmail.com>
2025-05-06 21:44:40 +0530

    initial commit

---
+++ src/test/java/com/selva/MovieReview_app_service/MovieReviewAppServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selva.MovieReview_app_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReviewAppServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
