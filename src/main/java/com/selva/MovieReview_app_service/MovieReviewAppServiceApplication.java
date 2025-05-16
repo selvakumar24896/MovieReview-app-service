469f9366c6654d70cf7ec3e502bbaff9e293df1a
selvakumar24896 <selvakumar24896@gmail.com>
2025-05-16 09:28:45 +0530

    added mongo connection and test Dummy api

--- src/main/java/com/selva/MovieReview_app_service/MovieReviewAppServiceApplication.java
+++ src/main/java/com/selva/MovieReview_app_service/MovieReviewAppServiceApplication.java
@@ -2,12 +2,19 @@ package com.selva.MovieReview_app_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MovieReviewAppServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieReviewAppServiceApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String appRoot(){
+		return  "Hello, world.";
+	}
 }
