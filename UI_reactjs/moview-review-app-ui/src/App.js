e2f8cf74e13e40105c511c0544106390ff526f0e
selvakumar24896 <selvakumar24896@gmail.com>
2025-05-26 06:56:32 +0530

    UI: test get all  movies api

--- UI_reactjs/moview-review-app-ui/src/App.js
+++ UI_reactjs/moview-review-app-ui/src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import api from './api/axiosConfig';
 
 function App() {
+
+  const [movies, setMovies] = useState();
+
+  const getMovies = async () => {
+    try {
+      const response = await api.get("api/v1/movies/all");
+      console.log(response.data);
+      setMovies(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
